8642fea03dcfeca3987aa41657c94cf9ec3fb4c2
席子良 <934887857@qq.com>
2022-09-25 12:59:53 +0800

    chore: new  add unocss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { $router } from '@/router'
+import "./styles/index.scss"
+import 'uno.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="node" />
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import * as path from 'path'
-import pluginTs from 'vite-plugin-ts';
+import PluginTs from 'vite-plugin-ts'
+import Unocss from 'unocss/vite'
 
 const rootPath = __dirname;
 
@@ -14,7 +15,13 @@ export default defineConfig({
       '@': `${path.resolve(rootPath, 'src')}`,
     },
   },
-  plugins: [vue(), pluginTs()],
+  plugins: [
+    Vue(), 
+    PluginTs(),
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
   server: {
     host: '0.0.0.0',
     open: true,
