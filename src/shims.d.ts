e264d031c49e1069c2100f36fdd1e76d4f2f031f
席子良 <934887857@qq.com>
2022-09-28 20:05:36 +0800

    chore: update global type file

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,9 @@
-/// <reference types="vite/client" />
+declare interface Window {
+  // extend the window
+}
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+  import { type DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
