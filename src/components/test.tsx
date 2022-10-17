beb160b84621939e05d95af9bf894b4e42e204bb
席子良 <934887857@qq.com>
2022-10-17 21:55:26 +0800

    chore: update package

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,7 +1,7 @@
 import { ref, defineComponent, Fragment } from 'vue';
 
 export const Test = defineComponent({
-  setup(props, { emit, expose, slots }) {
+  setup(props, { slots }) {
     const count = ref(0);
     const countRef = ref();
     const defaultSlot = slots.default?.();
