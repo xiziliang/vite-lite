812ccda9bbe6d7ae5914b23d7d2a57c8acf7ba69
席子良 <934887857@qq.com>
2022-11-29 09:47:38 +0800

    feat: complete commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,13 @@ export default {
     "@commitlint/config-conventional"
   ],
   rules: {
+    // array[0]:  (level) 0 disable 1 warning 2 error
+    // array[1]:  (always | never) never: inverts the rule
+    // array[2]:  (value)
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
     'type-enum': [
       2,
       'always',
